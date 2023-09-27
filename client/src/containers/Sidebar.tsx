4808ebab5d1c886bc51cf906c0372aae52d7a421
Sushil-cmd-r <kandharesushil10@gmail.com>
2023-09-27 22:44:55 +0530

    Sidebar Done

--- client/src/containers/Sidebar.tsx
+++ client/src/containers/Sidebar.tsx
@@ -1,13 +1,48 @@
+import image from '../assets/images/image.webp'
+import Pagination from '../components/Pagination'
+
 const Sidebar = () => {
   return (
-    <aside className="  hidden sm:block sm:col-span-1 z-3 flex flex-col">
-      <section className="h-28">
-        Recent Blogs
+    <aside className=" sm:col-span-1 z-3 flex flex-col">
+      <section className="hidden sm:flex flex-col items-center mb-8">
+        <span className="text-2xl font-medium">POPULAR BLOGS</span>
+        <hr className="w-full my-2 border-t-2" />
+        <ul>
+          <li className='flex p-1 gap-2 cursor-pointer group items-center  justify-center'>
+            <div className='w-[30%] md:w-[40%] lg:w-[25%] h-full rounded-md overflow-hidden'>
+              <img src={image} className='w-full object-cover' />
+            </div>
+            <p className='w-[70%] md:w-[60%] lg:w-[75%] line-clamp-2  group-hover:underline'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </li>
+
+          <li className='flex p-1 gap-2 cursor-pointer group items-center  justify-center'>
+            <div className='w-[30%] md:w-[40%] lg:w-[25%] h-full rounded-md overflow-hidden'>
+              <img src={image} className='w-full object-cover' />
+            </div>
+            <p className='w-[70%] md:w-[60%] lg:w-[75%] line-clamp-2  group-hover:underline'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </li>
+
+          <li className='flex p-1 gap-2 cursor-pointer group items-center  justify-center'>
+            <div className='w-[30%] md:w-[40%] lg:w-[25%] h-full rounded-md overflow-hidden'>
+              <img src={image} className='w-full object-cover' />
+            </div>
+            <p className='w-[70%] md:w-[60%] lg:w-[75%] line-clamp-2  group-hover:underline'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </li>
+
+        </ul>
+
       </section>
-      <div className="h-28 sticky top-16">
-        Pagination
-      </div>
-    </aside>
+      {/* Pagination */}
+      <section className="flex justify-center sticky top-20">
+        <Pagination />
+      </section>
+    </aside >
   )
 }
 export default Sidebar
